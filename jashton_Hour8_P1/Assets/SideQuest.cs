a4543ac02e4f54f5d76c3f88741efd276184c7c0
Joe Ashton <jashton001@student.sdccd.edu>
2025-10-23 17:55:41 -0700

    Hour 8_Part 1

---
+++ jashton_Hour8_P1/Assets/SideQuest.cs
@@ -0,0 +1,23 @@
+/* 
+ * int TakeDamage(int damageAmount) 
+ * {
+ *      int health = 100; 
+ *      return health - damageAmount; 
+ *      
+ * }
+ * 
+ */
+
+/* 1. What's the method's name? 
+ *    - The method's name is called "TakeDamage". 
+ *    
+ * 2. What variable type does the method return?
+ *      - The variable type the method returns is the "integer" (int). 
+ *      
+ * 3. How many parameters does the method have,a and what are they? 
+ *      - The method has one parameter, and it is called "damageAmount".
+ *      
+ * 4. What code is in the method's block?
+ *      - The code that is in the method's block is health and damageAmount.
+ *      
+ */ 
